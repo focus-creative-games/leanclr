a71369b9476742b7b1d968425175dcd5a3f49cd3
absences <49706424+absences@users.noreply.github.com>
2026-01-23 10:45:10 +0800

    support visual studio 2026

--- src/runtime/build.bat
+++ src/runtime/build.bat
@@ -4,7 +4,7 @@ setlocal
 :: Parse command line arguments
 set BUILD_TYPE=Release
 set ARCH=x64
-set VS_VERSION=17 2022
+set VS_VERSION=17
 set CLEAN=0
 set BUILD_SHARED=0
 
@@ -17,10 +17,14 @@ if /i "%~1"=="-clean" set CLEAN=1
 if /i "%~1"=="x86" set ARCH=Win32
 if /i "%~1"=="x64" set ARCH=x64
 if /i "%~1"=="shared" set BUILD_SHARED=1
+if /i "%~1"=="vs_version"  set VS_VERSION=%~2
 shift
 goto parse_args
 :end_parse
-
+if defined VS_VERSION (
+    if "%VS_VERSION%"=="17" set VS_YEAR=2022
+    if "%VS_VERSION%"=="18" set VS_YEAR=2026
+)
 :: Build directory
 set BUILD_DIR=%~dp0build
 set CMAKE_BUILD_DIR=%BUILD_DIR%\%BUILD_TYPE%
@@ -50,9 +54,9 @@ pushd "%CMAKE_BUILD_DIR%"
 echo.
 echo [1/2] Generating Visual Studio solution...
 if %BUILD_SHARED%==1 (
-    cmake -G "Visual Studio %VS_VERSION%" -A %ARCH% -DBUILD_SHARED_LEANCLR=ON ..\..\
+    cmake -G "Visual Studio %VS_VERSION% %VS_YEAR%" -A %ARCH% -DBUILD_SHARED_LEANCLR=ON ..\..\
 ) else (
-    cmake -G "Visual Studio %VS_VERSION%" -A %ARCH% ..\..\
+    cmake -G "Visual Studio %VS_VERSION% %VS_YEAR%" -A %ARCH% ..\..\
 )
 if errorlevel 1 (
     echo ERROR: CMake generation failed.
