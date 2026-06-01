e93edb9b9a7e2f4482646a187defbd15ebc45011
walon <walon@code-philosophy.com>
2026-06-01 16:46:23 +0800

    create arena for small object

--- src/runtime/gc/mark_sweep/mark_sweep_heap.cpp
+++ src/runtime/gc/mark_sweep/mark_sweep_heap.cpp
@@ -29,10 +29,10 @@ struct ArenaHeader
 {
     void* next_arena;
     FreeBlockHeader* free_list;
-    uint16_t arena_size;
-    uint16_t block_size;
-    uint16_t block_count;
-    // uint16_t used_count;
+    size_t arena_size;
+    size_t block_size;
+    size_t block_count;
+    // size_t used_count;
 };
 
 class SmallHeapArena
@@ -40,7 +40,7 @@ class SmallHeapArena
   private:
     ArenaHeader _header;
 
-    void initialize_free_list(uint16_t first_block_offset)
+    void initialize_free_list(size_t first_block_offset)
     {
         uint8_t* arena_data_start = (uint8_t*)this;
         FreeBlockHeader* first_block = (FreeBlockHeader*)(arena_data_start + first_block_offset);
@@ -49,7 +49,7 @@ class SmallHeapArena
         // block_offset is the offset of the current block from the arena_data_start,
         // so it includes the size of the ArenaHeader.
         FreeBlockHeader* cur_block = first_block;
-        for (uint16_t i = 0; i + 1 < _header.block_count; i++)
+        for (size_t i = 0; i + 1 < _header.block_count; i++)
         {
             FreeBlockHeader* next_block = (FreeBlockHeader*)((uint8_t*)(cur_block) + _header.block_size);
             cur_block->next_free = next_block;
@@ -59,7 +59,7 @@ class SmallHeapArena
     }
 
   public:
-    SmallHeapArena(uint16_t arena_size, uint16_t block_size, uint16_t block_alignment)
+    SmallHeapArena(size_t arena_size, size_t block_size, size_t block_alignment)
     {
         assert((void*)this == (void*)&_header);
         assert(arena_size % block_alignment == 0);
@@ -70,8 +70,8 @@ class SmallHeapArena
         _header.next_arena = nullptr;
         _header.arena_size = arena_size;
         _header.block_size = block_size;
-        uint16_t fist_block_offset = static_cast<uint16_t>(utils::MemOp::align_up(sizeof(ArenaHeader), block_alignment));
-        _header.block_count = (uint16_t)((size_t)arena_size - fist_block_offset) / block_size;
+        size_t fist_block_offset = utils::MemOp::align_up(sizeof(ArenaHeader), block_alignment);
+        _header.block_count = (arena_size - fist_block_offset) / block_size;
         // _header.used_count = 0;
         _header.free_list = nullptr;
         initialize_free_list(fist_block_offset);
@@ -103,12 +103,12 @@ template <>
 class ArenaAllocator<SmallHeapArena>
 {
   private:
-    uint16_t _arena_size;
-    uint16_t _block_size;
-    uint16_t _block_alignment;
+    size_t _arena_size;
+    size_t _block_size;
+    size_t _block_alignment;
 
   public:
-    ArenaAllocator(uint16_t arena_size, uint16_t block_size, uint16_t block_alignment)
+    ArenaAllocator(size_t arena_size, size_t block_size, size_t block_alignment)
         : _arena_size(arena_size), _block_size(block_size), _block_alignment(block_alignment)
     {
     }
@@ -141,20 +141,22 @@ class ArenaCollection
     ArenaAllocator<T> _allocator;
 
   public:
-    ArenaCollection(ArenaAllocator<T> allocator) : _allocator(allocator)
+    ArenaCollection(ArenaAllocator<T> allocator) : _allocator(allocator), _current_arena(nullptr)
     {
-        _current_arena = _allocator.allocate_arena();
     }
 
     void* allocate_block()
     {
-        void* block = _current_arena->allocate_block();
-        if (LEANCLR_LIKELY(block != nullptr))
+        if (LEANCLR_LIKELY(_current_arena != nullptr))
         {
-            return block;
+            void* block = _current_arena->allocate_block();
+            if (LEANCLR_LIKELY(block != nullptr))
+            {
+                return block;
+            }
+            _full_arenas.push_back(_current_arena);
+            _current_arena = nullptr;
         }
-
-        _full_arenas.push_back(_current_arena);
         if (!_not_full_arenas.empty())
         {
             _current_arena = _not_full_arenas.back();
@@ -165,8 +167,6 @@ class ArenaCollection
             T* new_arena = _allocator.allocate_arena();
             if (new_arena == nullptr)
             {
-                // Rollback the full arena
-                _full_arenas.pop_back();
                 return nullptr;
             }
             _current_arena = new_arena;
@@ -175,28 +175,76 @@ class ArenaCollection
     }
 };
 
-constexpr size_t kMinSmallHeapBlockSize = 8;
-constexpr size_t kMaxSmallHeapBlockSize = 256;
-constexpr size_t kSmallHeapBlockSizeIncrement = 8;
-constexpr size_t kSmallHeapArenaSize = 16 * 1024;
-static_assert(kSmallHeapArenaSize <= (1 << 16), "kSmallHeapArenaSize must be less than or equal to 64KB");
-constexpr size_t kSmallHeapArenaCount = (kMaxSmallHeapBlockSize - kMinSmallHeapBlockSize) / kSmallHeapBlockSizeIncrement + 1;
+struct ArenaCollectionInfo
+{
+    size_t max_size;
+    size_t size_increment;
+    size_t arena_size;
+};
 
-ArenaCollection<SmallHeapArena>* s_small_heap_arenas[kSmallHeapArenaCount] = {};
+static constexpr ArenaCollectionInfo s_arena_collection_infos[] = {
+    {256, 8, 16 * 1024},
+    {512, 16, 32 * 1024},
+    {1024, 32, 64 * 1024},
+    {2048, 64, 128 * 1024},
+    {4096, 128, 256 * 1024},
+    {8192, 256, 512 * 1024},
+};
+
+constexpr size_t kArenaCollectionInfoCount = sizeof(s_arena_collection_infos) / sizeof(s_arena_collection_infos[0]);
+constexpr size_t kArenaCollectionCount = kArenaCollectionInfoCount * 16 + 16 /* collection0 has extra 16 arenas for small objects */;
+static ArenaCollection<SmallHeapArena>* s_small_heap_arenas[kArenaCollectionCount] = {};
+constexpr size_t kMaxSmallObejctSize = s_arena_collection_infos[kArenaCollectionInfoCount - 1].max_size;
+static ArenaCollection<SmallHeapArena>* s_map_div8_size_to_arena[kMaxSmallObejctSize / 8] = {};
+
+// constexpr size_t kMinSmallHeapBlockSize = 8;
+// constexpr size_t kMaxSmallHeapBlockSize = 256;
+// constexpr size_t kSmallHeapBlockSizeIncrement = GC_ALIGN;
+// constexpr size_t kSmallHeapArenaSize = 16 * 1024;
+// static_assert(kSmallHeapArenaSize <= (1 << 16), "kSmallHeapArenaSize must be less than or equal to 64KB");
+// constexpr size_t kSmallHeapArenaCount = (kMaxSmallHeapBlockSize - kMinSmallHeapBlockSize) / kSmallHeapBlockSizeIncrement + 1;
 
 std::unordered_map<void*, size_t> s_fixed_blocks;
 
 static int64_t s_used_bytes = 0;
 static int64_t s_heap_bytes = 0;
 
-void initialize_small_heap_arenas()
+static void initialize_small_heap_arenas()
 {
-    for (size_t i = 0; i < kSmallHeapArenaCount; i++)
+    size_t last_arena_max_size = 0;
+    size_t last_arena_index = 0;
+    size_t last_map_index = 0;
+    for (size_t i = 0; i < kArenaCollectionInfoCount; i++)
     {
-        s_small_heap_arenas[i] = new ArenaCollection<SmallHeapArena>(
-            ArenaAllocator<SmallHeapArena>(static_cast<uint16_t>(kSmallHeapArenaSize),
-                                           static_cast<uint16_t>(kMinSmallHeapBlockSize + i * kSmallHeapBlockSizeIncrement), static_cast<uint16_t>(GC_ALIGN)));
+        size_t arena_size = s_arena_collection_infos[i].arena_size;
+        size_t max_size = s_arena_collection_infos[i].max_size;
+        size_t size_increment = s_arena_collection_infos[i].size_increment;
+        for (size_t j = 1; j <= (max_size - last_arena_max_size) / size_increment; j++)
+        {
+            size_t current_arena_size = last_arena_max_size + size_increment * j;
+            auto new_arena = new ArenaCollection<SmallHeapArena>(
+                ArenaAllocator<SmallHeapArena>(arena_size, current_arena_size, GC_ALIGN));
+            for (; last_map_index < current_arena_size / 8; last_map_index++)
+            {
+                s_map_div8_size_to_arena[last_map_index] = new_arena;
+            }
+            s_small_heap_arenas[last_arena_index++] = new_arena;
+        }
+        last_arena_max_size = max_size;
     }
+    assert (last_map_index == kMaxSmallObejctSize / 8);
+    assert (last_arena_index == kArenaCollectionCount);
+}
+
+static inline ArenaCollection<SmallHeapArena>* get_arena_collection(size_t size)
+{
+    assert (size > 0 && size % 8 == 0);
+    return s_map_div8_size_to_arena[size / 8 - 1];
+}
+
+static inline bool is_small_object(size_t size)
+{
+    return size <= kMaxSmallObejctSize;
 }
 
 static void scan_fixed_blocks(GcVisitUnknownBlock visit, void* userdata)
@@ -288,8 +336,6 @@ void MarkSweepHeap::free_fixed(void* address)
     alloc::GeneralAllocation::free(address);
 }
 
-
-
 vm::RtObject* MarkSweepHeap::allocate_object(const metadata::RtClass* klass, size_t size, const GcAllocSite& site)
 {
     return allocate_object(klass, size);
@@ -300,13 +346,17 @@ vm::RtObject* MarkSweepHeap::allocate_object(const metadata::RtClass* klass, siz
     assert(size >= sizeof(vm::RtObject));
     size_t aligned_size = utils::MemOp::align_up(size, GC_ALIGN);
     vm::RtObject* obj;
-    if (aligned_size <= kMaxSmallHeapBlockSize)
+    if (is_small_object(aligned_size))
     {
-        obj = (vm::RtObject*)s_small_heap_arenas[(aligned_size - kMinSmallHeapBlockSize) / kSmallHeapBlockSizeIncrement]->allocate_block();
+        obj = (vm::RtObject*)get_arena_collection(aligned_size)->allocate_block();
     }
     else
     {
         obj = (vm::RtObject*)alloc::GeneralAllocation::malloc_zeroed(aligned_size);
+    }
+    if (obj == nullptr)
+    {
+        return nullptr;
     }
     obj->klass = const_cast<metadata::RtClass*>(klass);
     s_used_bytes += aligned_size;
