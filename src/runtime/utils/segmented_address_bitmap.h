dbc1924ca81edbebee04f1349f5cdd14ae5334a8
walon <walon@code-philosophy.com>
2026-06-08 11:36:39 +0800

    refactor(runtime): refactor SegmentedAddressBitmap::kSegmentCacheSize as template argument SEGMENT_CACHE_SIZE

--- src/runtime/utils/segmented_address_bitmap.h
+++ src/runtime/utils/segmented_address_bitmap.h
@@ -11,20 +11,19 @@ namespace utils
 {
 
 // Sparse bitmap keyed by address / slot_granularity. mark() returns true on first visit.
-template <size_t SLOT_GRANULARITY, size_t SEGMENT_SIZE_BYTE_COUNT>
+template <size_t SLOT_GRANULARITY, size_t SEGMENT_SIZE_BYTE_COUNT, size_t SEGMENT_CACHE_SIZE>
 class SegmentedAddressBitmap
 {
   private:
     static_assert((SLOT_GRANULARITY & (SLOT_GRANULARITY - 1)) == 0, "SLOT_GRANULARITY must be a power of two");
     static_assert((SEGMENT_SIZE_BYTE_COUNT & (SEGMENT_SIZE_BYTE_COUNT - 1)) == 0, "SEGMENT_SIZE_BYTE_COUNT must be a power of two");
     static_assert(SEGMENT_SIZE_BYTE_COUNT % sizeof(size_t) == 0, "SEGMENT_SIZE_BYTE_COUNT must be a multiple of sizeof(size_t)");
+    static_assert((SEGMENT_CACHE_SIZE & (SEGMENT_CACHE_SIZE - 1)) == 0, "SEGMENT_CACHE_SIZE must be a power of two");
 
     static constexpr size_t kBitsPerWord = sizeof(size_t) * 8;
     static constexpr size_t kSegmentWordCount = SEGMENT_SIZE_BYTE_COUNT / sizeof(size_t);
     static constexpr size_t kSegmentBitCount = kSegmentWordCount * kBitsPerWord;
     static constexpr size_t kInvalidSegmentIndex = static_cast<size_t>(-1);
-    static constexpr size_t kSegmentCacheSize = 1024;
-    static_assert((kSegmentCacheSize & (kSegmentCacheSize - 1)) == 0, "kSegmentCacheSize must be a power of two");
 
     struct SegmentCacheEntry
     {
@@ -33,7 +32,7 @@ class SegmentedAddressBitmap
     };
 
     HashMap<size_t, size_t*> _segment_map;
-    mutable SegmentCacheEntry _segment_cache[kSegmentCacheSize]{};
+    mutable SegmentCacheEntry _segment_cache[SEGMENT_CACHE_SIZE]{};
 
     size_t* get_or_create_segment_slow(size_t segment_index, size_t cache_slot)
     {
@@ -50,7 +49,7 @@ class SegmentedAddressBitmap
 
     size_t* get_or_create_segment(size_t segment_index)
     {
-        const size_t cache_slot = segment_index & (kSegmentCacheSize - 1);
+        const size_t cache_slot = segment_index & (SEGMENT_CACHE_SIZE - 1);
         const SegmentCacheEntry& entry = _segment_cache[cache_slot];
         if (entry.segment_index == segment_index)
         {
