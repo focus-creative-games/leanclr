b3a0e0bd89ad8d9c6f648d3549f33dacd8ffd3d0
walon <walon@code-philosophy.com>
2026-06-08 20:36:44 +0800

    test: add test for System.Span<T>::get_Item

---
+++ src/tests/managed/CorlibTests/Intrinsic/TC_Syste_Span.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tests.Intrinsic
+{
+    internal class TC_Syste_Span : TestCaseBase
+    {
+        [UnitTest]
+        public unsafe void GetItem()
+        {
+            byte[] spanBytes = new byte[2];
+            Span<byte> span = spanBytes;
+            span[0] = 145;
+            span[1] = 99;
+            Assert.Equal(145, spanBytes[0]);
+            Assert.Equal(99, spanBytes[1]);
+        }
+    }
+}
