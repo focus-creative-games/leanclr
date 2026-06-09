984d3d1b69171e26e770c9511621b2c8ae07584e
walon <walon@code-philosophy.com>
2026-06-09 13:16:09 +0800

    feat(runtime): setup gc bitmap for class static fields

--- src/runtime/gc/obj_scan_util.h
+++ src/runtime/gc/obj_scan_util.h
@@ -24,82 +24,11 @@ public:
     template<typename VisitContext>
     static void visit_class_static_data(const metadata::RtClass* klass, VisitContext& ctx)
     {
-        for (uint16_t i = 0; i < klass->field_count; ++i)
+        if (klass->static_fields_data == nullptr || klass->static_gc_bitmap_word_count == 0)
         {
-            const metadata::RtFieldInfo* field = klass->fields + i;
-            if (!vm::Field::is_static_excluded_literal_and_rva(field))
-            {
-                continue;
-            }
-            const metadata::RtTypeSig* type_sig = field->type_sig;
-            if (type_sig->is_by_ref())
-            {
-                continue;
-            }
-            switch (type_sig->ele_type)
-            {
-            case metadata::RtElementType::Boolean:
-            case metadata::RtElementType::Char:
-            case metadata::RtElementType::I1:
-            case metadata::RtElementType::U1:
-            case metadata::RtElementType::I2:
-            case metadata::RtElementType::U2:
-            case metadata::RtElementType::I4:
-            case metadata::RtElementType::U4:
-            case metadata::RtElementType::I8:
-            case metadata::RtElementType::U8:
-            case metadata::RtElementType::R4:
-            case metadata::RtElementType::R8:
-            case metadata::RtElementType::I:
-            case metadata::RtElementType::U:
-            case metadata::RtElementType::Ptr:
-            case metadata::RtElementType::FnPtr:
-            case metadata::RtElementType::TypedByRef:
-            case metadata::RtElementType::Var:
-            case metadata::RtElementType::MVar:
-            {
-                break;
-            }
-            case metadata::RtElementType::Object:
-            case metadata::RtElementType::String:
-            case metadata::RtElementType::Class:
-            case metadata::RtElementType::SZArray:
-            case metadata::RtElementType::Array:
-            handle_reference_type_field:
-            {
-                assert(field->offset % sizeof(void*) == 0);
-                vm::RtObject* obj = *reinterpret_cast<vm::RtObject**>(reinterpret_cast<uint8_t*>(klass->static_fields_data) + field->offset);
-                visit_child(obj, ctx);
-                break;
-            }
-            case metadata::RtElementType::GenericInst:
-            {
-                bool is_ref_type = vm::Type::is_reference_type(type_sig).unwrap();
-                if (is_ref_type)
-                {
-                    goto handle_reference_type_field;
-                }
-                // fall through
-            }
-            case metadata::RtElementType::ValueType:
-            {
-                metadata::RtClass* field_class = vm::Class::get_class_from_typesig(type_sig).unwrap();
-                assert(vm::Class::has_initialized_part(field_class, metadata::RtClassInitPart::Field));
-                if (!vm::Class::get_has_references(field_class))
-                {
-                    break;
-                }
-                assert(field->offset % sizeof(void*) == 0);
-                uint8_t* cur_field_address = reinterpret_cast<uint8_t*>(klass->static_fields_data) + field->offset;
-                visit_value_type(cur_field_address, ctx, field_class);
-                break;
-            }
-            default:
-            {
-                assert(false && "invalid element type");
-            }
-            }
+            return;
         }
+        visit_gc_bitmap_words(klass->static_gc_bitmap, klass->static_gc_bitmap_word_count, klass->static_fields_data, ctx);
     }
 
     template<typename VisitContext>
@@ -181,13 +110,17 @@ private:
     template<typename VisitContext>
     static void visit_gc_bitmap(const metadata::RtClass* klass, uint8_t* slot_base, VisitContext& ctx)
     {
-        const size_t word_count = klass->gc_bitmap_word_count;
+        visit_gc_bitmap_words(klass->gc_bitmap, klass->gc_bitmap_word_count, slot_base, ctx);
+    }
+
+    template<typename VisitContext>
+    static void visit_gc_bitmap_words(const size_t* bitmap, size_t word_count, uint8_t* slot_base, VisitContext& ctx)
+    {
         if (word_count == 0)
         {
             return;
         }
 
-        const size_t* bitmap = klass->gc_bitmap;
         const size_t kBitsPerWord = vm::Class::kBitsPerWord;
 
         for (size_t w = 0; w < word_count; ++w)
